76c3d66b2c75a1120750f53020c73802632adf04
Jaquier06 <80239710+Jaquier06@users.noreply.github.com>
2024-03-10 20:04:08 +0100

    Update configConnexion.php

--- projet/server/wrk/configConnexion.php
+++ projet/server/wrk/configConnexion.php
@@ -1,6 +1,17 @@
 <?php
+
+// LOCAL :
+/*
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'host.docker.internal');
 define('DB_NAME', 'db_151');
 define('DB_USER', 'root');
 define('DB_PASS', 'emf123');
+*/
+
+// ONLINE
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'jaquierl.emf-informatique.ch');
+define('DB_NAME', 'jaquierl_151');
+define('DB_USER', 'jaquierl_adm');
+define('DB_PASS', 'cjk9KVhQ127g');
